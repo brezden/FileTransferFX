194256048dc8740e9015fcdc92005d0c6318ee90
brezden <callumbrezden@gmail.com>
2022-09-12 22:09:05 -0700

    Added CSS and Menu Bar (v0.0.2)
    Connected stylesheets to the program and added a menu bar to the program.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,11 @@
 package application;
 	
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
@@ -18,8 +13,14 @@ public class Main extends Application {
 	
 		Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
 		Scene scene = new Scene(root);
+		String css = this.getClass().getResource("/stylesheets/application.css").toExternalForm();
+		scene.getStylesheets().add(css);
+		
 		primaryStage.setTitle("File Transfer FX");
 		primaryStage.setScene(scene);
+		primaryStage.setHeight(650);
+		primaryStage.setWidth(900);
+		primaryStage.setResizable(false);
 		
 		Image programIcon = new Image("images/fileTransferFXLogo.png");
 		primaryStage.getIcons().add(programIcon);
