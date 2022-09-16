0cfe096d3f753d74e06c4d95de3e34574dbf1cd1
brezden <callumbrezden@gmail.com>
2022-09-15 21:18:05 -0700

    Added File Choice and List View (v0.0.3)
    Created a way for the user to add files to a list that the program can backup in a new folder or overwrite files.

--- src/application/MainSceneController.java
+++ src/application/MainSceneController.java
@@ -1,5 +1,34 @@
 package application;
 
-public class MainSceneController {
+import java.io.File;
+import java.util.List;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+
+public class MainSceneController {
+	
+	@FXML
+	private Button btn2;
+	
+	@FXML
+	private ListView fileListView;
+
+	public void Button2Action(ActionEvent event) {
+		FileChooser fc = new FileChooser();
+		List<File> selectedFiles = fc.showOpenMultipleDialog(null);
+		
+		if (selectedFiles != null) {
+			for (int i = 0; i < selectedFiles.size(); i++) {
+				fileListView.getItems().add(selectedFiles.get(i).getAbsolutePath());
+			}
+		}
+		else {
+			System.out.println("File is not valid!");
+		}
+	}
+	
 }
